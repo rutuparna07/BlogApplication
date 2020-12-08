dc7fca8c3fe0b2ec52b9118ae0f0ab1d5d698d64
Bishal-Nakoda <bishalnakoda37@gmail.com>
2020-12-08 13:44:40 +0530

    categories 1.2 fixed

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,6 @@ Route::group(['middleware' => ['auth','admin']], function(){
     Route::name('categories.index')->get('/categories', 'CategoryController@index');
     Route::name('categories.store')->post('/categories','CategoryController@store');
     Route::name('categories.update')->put('/categories/{id}','CategoryController@update');
-    Route::name('categories.getid')->get('/categorysearch','CategoryController@getid');
-    Route::name('categories.search')->get('/categorysearch/{id}','CategoryController@categorysearch');
     Route::name('categories.delete')->delete('/categories/{id}','CategoryController@destroy');
 
 });
@@ -62,4 +60,7 @@ Route::name('about-us')->get('/about-us', function () {
 });
 
 Route::get('search','Admin\DashboardController@search');
+Route::name('categories.getid')->get('/categorysearch','CategoryController@getid');
+Route::name('categories.search')->get('/categorysearch/{id}','CategoryController@categorysearch');
+
 
